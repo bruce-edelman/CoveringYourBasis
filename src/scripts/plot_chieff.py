7e4c8170205d0259ff73d9f79c4f97d23adb9ecf
Bruce Edelman <bedelman@uoregon.edu>
2022-09-28 08:43:29 -0700

    plot o3b gaussian chieff

--- src/scripts/plot_chieff.py
+++ src/scripts/plot_chieff.py
@@ -2,6 +2,7 @@ import paths
 import numpy as np
 import matplotlib.pyplot as plt
 import deepdish as dd
+import json
 from utils import plot_mean_and_90CI
 
 fig, ax = plt.subplots(1,1,figsize=(7,5))
@@ -22,6 +23,13 @@ for i in range(1500):
     norm = np.trapz(pchiefss[i, :], x=xs)
     pchiefss[i, :] /= norm
 ax = plot_mean_and_90CI(ax, xs, pchiefss, color='tab:red', label='MSpline Chi Eff')
+
+with open(paths.data / "gaussian-spin-xeff-xp-ppd-data.json", 'r') as jf:
+    o3b_gaussian_data = json.load(jf)
+    o3b_gaussian_chi_eff_grid = np.array(o3b_gaussian_data['chi_eff_grid'])
+    o3b_gaussian_chi_eff_data = np.array(o3b_gaussian_data['chi_eff_pdfs'])
+ax = plot_mean_and_90CI(ax, o3b_gaussian_chi_eff_grid, o3b_gaussian_chi_eff_data, color='tab:olive', label='Gaussian', bounds=False)
+
 plt.xlim(-0.75, 0.5)
 plt.ylim(0, 6.5)
 plt.legend(frameon=False, fontsize=14)
